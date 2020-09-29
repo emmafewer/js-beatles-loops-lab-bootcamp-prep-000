8a5879974345b4c0517ae53ef76e8af6eb486b6d
Emma Fewer <emmafewer20@gmail.com>
2020-09-29 18:46:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
+<<<<<<< HEAD
  var facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
   
  function johnLennonFacts(facts) {
@@ -19,6 +20,18 @@ function theBeatlesPlay(musicians, instruments) {
   }
   
 johnLennonFacts(facts);
+=======
+function johnLennonFacts() {
+  var facts = ['While on tour, he enjoyed playing Monopoly', 'Harmonica was the first instrument Lennon learned to play'];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+>>>>>>> 5b50bb6169e96c9418b84f59c890563549866bf7
 
 function iLoveTheBeatles(number) {
   var array = [];
